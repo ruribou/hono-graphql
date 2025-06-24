333df4e164903f1720fdba0a23e5acf55ac54c14
ruribou <ruribou@hotmail.com>
2025-06-24 22:37:57 +0900

    feat: 一旦GraphQLの動作確認ができたのでcommit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
+import { graphqlServer, type RootResolver } from '@hono/graphql-server'
+import { buildSchema } from 'graphql'
 import { Hono } from 'hono'
+import { schema as originalSchema } from './graphql/schema'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const schema = buildSchema(originalSchema)
+
+const rootResolver: RootResolver = () => {
+  return {
+    hello: () => 'Hello Hono GraphQL!!',
+  };
+};
+
+app.use(
+  '/graphql',
+  graphqlServer({
+    schema,
+    rootResolver,
+    graphiql: true,
+  })
+)
+
+if (require.main === module) {
+  Bun.serve({
+    fetch: app.fetch,
+    port: 3000,
+  })
+}
 
 export default app
